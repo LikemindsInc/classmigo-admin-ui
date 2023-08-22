4dea4c272b143fcf6891a115340112382905bc39
charles <charles.onuorah@yahoo.com>
2023-08-22 09:23:31 +0100

    add features

--- src/utils/dummyDataParents.tsx
+++ src/utils/dummyDataParents.tsx
@@ -1,50 +1,9 @@
 import { ColumnsType } from "antd/es/table";
 import { UserDetails } from "../Pages/PrivatePages/Dashboard/Users/Parents/Components/UserDetails";
 import { Options } from "../Ui_elements";
+import { IStudent } from "@appModel";
 
-interface DataType {
-  key: string;
-  name: string;
-  email: string;
-  phoneNumber: number;
-  status: string;
-}
-
-export const columns: ColumnsType<DataType> = [
-  {
-    title: "NAME",
-    dataIndex: "name",
-    key: "name",
-    render: (name: string) => (
-      <UserDetails name={name} />
-    ),
-  },
-  {
-    title: "EMAIL",
-    dataIndex: "email",
-    key: "email",
-  },
-  {
-    title: "PHONE NUMBER",
-    dataIndex: "phoneNumber",
-    key: "phoneNumber",
-  },
-  {
-    title: "STATUS",
-    dataIndex: "status",
-    key: "status",
-  },
-  {
-    title: "",
-    dataIndex: "options",
-    key: "options",
-    render: () => (
-      <Options/>
-    ),
-  },
-];
-
-export const data: DataType[] = [
+export const data: any[] = [
   {
     key: "1",
     name: "John Brown",
